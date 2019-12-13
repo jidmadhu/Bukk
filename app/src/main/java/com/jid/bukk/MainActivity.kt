c07771596855529f856445f8c0f92305a7288d14
jmadhu <jid.madhu@gmail.com>
2019-12-13 14:29:10 +1000

    Second Commit from Mac

--- app/src/main/java/com/jid/bukk/MainActivity.kt
+++ app/src/main/java/com/jid/bukk/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.jid.bukk
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Placing a call
+
+
     }
 }
